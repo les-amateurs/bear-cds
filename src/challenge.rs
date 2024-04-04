c242bcbc20ecc34b92ee5ae98da6aecb8eb5a999
voxal <aidenhohoho@gmail.com>
2024-04-03 22:26:03 -0700

    env

--- src/challenge.rs
+++ src/challenge.rs
@@ -47,6 +47,7 @@ pub struct Container {
     pub build: PathBuf,
     pub limits: Limits,
     ports: Option<Vec<u32>>,
+    pub env: HashMap<String, String>,
 }
 
 // im honestly uncertain what types these should be so im using these
